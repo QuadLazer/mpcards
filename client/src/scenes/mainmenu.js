f6c693deed7e1b261145e6620634e2737512b0f7
John Scharff <jscharff@ufl.edu>
2024-04-02 17:17:55 -0400

    added functional logout button

--- client/src/scenes/mainmenu.js
+++ client/src/scenes/mainmenu.js
@@ -1,4 +1,6 @@
 import io from 'socket.io-client';
+import FirebasePlugin from '../plugins/FirebasePlugin';
+
 export default class MainMenu extends Phaser.Scene {
     constructor() {
         super('MainMenu')
@@ -9,21 +11,44 @@ export default class MainMenu extends Phaser.Scene {
         this.load.image('playButton','assets/menu_assets/playButton.png');
         this.load.image('profileButton','assets/menu_assets/profileButton.png');
         this.load.image('rankingsButton','assets/menu_assets/rankingsButton.png');
+        this.load.image('logOut', 'assets/menu_assets/logOut.png');
         this.load.image('bg', 'assets/bgtest.png');
     }
+    
 
-    create () {
+    create (loginFlag) {
+
+        var firebaseApp = this.plugins.get('FirebasePlugin');
+        
         this.bg = this.add.image(0, 0, 'bg');
         this.playButton = this.add.image(460, 500, 'playButton').setScale(0.75, 0.75).setInteractive();
         this.profileButton = this.add.image(960, 570, 'profileButton').setScale(0.75, 0.75).setInteractive();
         this.rankingsButton = this.add.image(950, 393, 'rankingsButton').setScale(0.75, 0.75).setInteractive();
         this.title = this.add.image(650, 150, 'title').setScale(0.75, 0.75).setInteractive();
+        this.logOut = this.add.image(60, 60, 'logOut').setScale(0.75, 0.75).setInteractive();
         Phaser.Display.Align.In.Center(this.bg, this.add.zone(640, 390, 1280, 780));
         
-        this.socket = io('http://localhost:3000');
+        if (this.socket == null) {
+            console.log("I was null!");
+            this.socket = io('http://localhost:3000');
+        } else {
+            console.log("I'm already set!");
+            this.socket.close();
+            this.socket = io('http://localhost:3000');
+        }
 
         this.playButton.on('pointerup', function (pointer) {
             this.scene.start('Loading', {socket: this.socket});
+        }, this);
+
+        this.logOut.on('pointerup', function (pointer) {
+            this.socket.disconnect();
+            firebaseApp.auth.signOut().then((firebaseApp) => {
+                console.log('Signed out');
+                this.scene.start('Login', loginFlag);
+            }).catch((error) => {
+                console.log(error);
+            });
         }, this);
 
         this.profileButton.on('pointerup', function (pointer) {
