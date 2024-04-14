caca6aff148b02bbe3a4eee1e9477a2f84999887
John Scharff <jscharff@ufl.edu>
2024-04-14 15:46:28 -0400

    maybe maybe maybe-

--- server/dbserver.js
+++ server/dbserver.js
@@ -8,7 +8,10 @@ const errorHandler = require("./errorHandler.js")
 
 const app = express();
 
-app.use(cors()); //avoid cross origin access errors
+app.use(cors({ 
+    origin: '*', 
+    credentials: true 
+})) //avoid cross origin access errors
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use("", router);
