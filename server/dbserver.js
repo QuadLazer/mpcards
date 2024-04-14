f39b8a42ae220de6167c1a06df928bca50d66e04
John Scharff <jscharff@ufl.edu>
2024-04-14 15:53:39 -0400

    geniunely tweaking rn

--- server/dbserver.js
+++ server/dbserver.js
@@ -8,10 +8,18 @@ const errorHandler = require("./errorHandler.js")
 
 const app = express();
 
-app.use(cors({ 
-    origin: '*', 
-    credentials: true 
-})) //avoid cross origin access errors
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = '*';
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  }); //avoid cross origin access errors
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use("", router);
--- server/server.js
+++ server/server.js
@@ -4,10 +4,31 @@ const http = require('http').createServer(express);
 const io = require("socket.io")(http);
 let players = [];
 
-app.use(cors({ 
-    origin: '*', 
-    credentials: true 
-}))
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = '*';
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
+
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:3000', 'http://gamebrag.onrender.com', 'https://gamebrag.onrender.com'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 
 app.use(express.static(__dirname + '/public'));
 
