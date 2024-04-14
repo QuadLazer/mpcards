bcf28f7771a042b6401cacd2c29e31e294387aec
John Scharff <jscharff@ufl.edu>
2024-04-14 16:14:33 -0400

    db fix

--- server/dbserver.js
+++ server/dbserver.js
@@ -8,18 +8,7 @@ const errorHandler = require("./errorHandler.js")
 
 const app = express();
 
-app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    const allowedOrigins = '*';
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-         res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
-  }); //avoid cross origin access errors
+app.use(cors()); //avoid cross origin access errors
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use("", router);
